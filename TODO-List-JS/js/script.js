6f5e7fdece8c96b32c4e701388f0a5b7c1b5f35d
Victor <vpanya97@gmail.com>
2022-06-02 18:21:16 +0300

    add the function of editing and deleting in list

--- TODO-List-JS/js/script.js
+++ TODO-List-JS/js/script.js
@@ -28,8 +28,19 @@ function createTodoItem(title) {
     listItem.appendChild(editButton);
     listItem.appendChild(deleteButton);
 
-    return listItem;
+    bindEvents(listItem);
 
+    return listItem;
+}
+
+function bindEvents(todoItem) {
+    const checkbox = todoItem.querySelector('.checkbox');
+    const editButton = todoItem.querySelector('button.edit');
+    const deleteButton = todoItem.querySelector('button.delete');
+
+    checkbox.addEventListener('change', toggleTodoItem);
+    editButton.addEventListener('click', editTodoItem);
+    deleteButton.addEventListener('click', deleteTodoItem);
 }
 
 function addTodoItem(event) {
@@ -41,6 +52,34 @@ function addTodoItem(event) {
     todoList.appendChild(todoItem);
     addInput.value = '';
 }
+
+function toggleTodoItem() {
+    const listItem = this.parentNode;
+    listItem.classList.toggle('completed');
+}
+
+function editTodoItem() {
+    const listItem = this.parentNode;
+    const title = listItem.querySelector('.title');
+    const editInput = listItem.querySelector('.textfield');
+    const isEditing = listItem.classList.contains('editing');
+
+    if (isEditing) {
+        title.innerText = editInput.value;
+        this.innerText = 'Изменить'; 
+    } else {
+        editInput.value = title.innerText;
+        this.innerText = 'Сохранить';
+    }
+
+    listItem.classList.toggle('editing');
+}
+
+function deleteTodoItem() {
+    const listItem = this.parentNode;
+    todoList.removeChild(listItem);
+}
+
 
 const todoForm = document.getElementById('todo-form');
 const addInput = document.getElementById('add-input');
