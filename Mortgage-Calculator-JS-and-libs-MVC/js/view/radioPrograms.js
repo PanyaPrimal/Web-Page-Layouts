2077b7711d7c07989ee7449ef4d903e7d7ac7b22
Victor <vpanya97@gmail.com>
2022-11-21 11:28:33 +0200

    move model updator  to a separate block

--- Mortgage-Calculator-JS-and-libs-MVC/js/view/radioPrograms.js
+++ Mortgage-Calculator-JS-and-libs-MVC/js/view/radioPrograms.js
@@ -1,3 +1,5 @@
+import updateModel from "./../utils/updateModel.js";
+
 function init(getData) {
 
     const radioBtns = document.querySelectorAll('input[name="program"]')
@@ -17,20 +19,12 @@ function init(getData) {
 
     radioBtns.forEach(function(radioBtn) {
         radioBtn.addEventListener('change', function() {
-            console.log(this);
-            console.log(parseFloat(this.value));
-            console.log(this.id);
+            updateModel(this, {
+                onUpdate: 'radioProgram',
+                selectedProgram: parseFloat(this.value),
+                id: this.id,
+            });
 
-            this.dispatchEvent(
-                new CustomEvent('updateForm', {
-                    bubbles: true,
-                    detail: {
-                            selectedProgram: parseFloat(this.value),
-                            onUpdate: 'radioProgram',
-                            id: this.id,
-                    },
-                })
-            );
         });
     });
 }
