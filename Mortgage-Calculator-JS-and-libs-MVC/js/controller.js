bea9fbcda91ba7952030839b3ee21ef5c0734734
Victor <vpanya97@gmail.com>
2022-12-15 12:38:12 +0200

    add paymentRange scale

--- Mortgage-Calculator-JS-and-libs-MVC/js/controller.js
+++ Mortgage-Calculator-JS-and-libs-MVC/js/controller.js
@@ -6,6 +6,7 @@ import { updateMinPercents } from "./view/utils.js";
 import costInput from './view/costInput.js';
 import costRange from './view/costRange.js';
 import paymentInput from './view/paymentInput.js';
+import paymentRange from './view/paymentRange.js';
 
 window.onload = function () {
 	const getData = Model.getData;
@@ -19,6 +20,7 @@ window.onload = function () {
 
     //init payment input
     const cleavePayment = paymentInput(getData);
+    const sliderPayment = paymentRange(getData);
 
 	document.addEventListener('updateForm', (e) => {
 		Model.setData(e.detail);
--- Mortgage-Calculator-JS-and-libs-MVC/js/model.js
+++ Mortgage-Calculator-JS-and-libs-MVC/js/model.js
@@ -63,6 +63,10 @@ function setData(newData) {
         }
 	}
 
+    if (newData.onUpdate === 'paymentSlider') {
+        newData.paymentPercents = newData.paymentPercents / 100;
+    }
+
     data = {
         ...data,
         ...newData,
